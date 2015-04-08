de4c09c576499619f96f7a1758f3baa843d0b042
markpj1 <markpjaramillo@gmail.com>
2015-04-07 21:28:22 -0700

    FizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+//fizzBuzz.js
+
+function fizzBuzz(start, end) {
+	
+	for(var i = start ; i <= end; i++) {
+		var count = "";
+		if (i % 3 === 0) {
+			count += "Fizz";
+		}
+		if (i % 5 === 0) {
+			count += "Buzz";
+		}
+		console.log(count || i);
+	};
+	
+	
+}
+fizzBuzz(1,100);
